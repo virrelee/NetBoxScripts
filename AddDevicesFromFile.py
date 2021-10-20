d179d5fc8e84d318242d9c341c821122633d6ef6
virrelee <viktor.lindgren@live.se>
2021-10-20 16:25:56 +0200

    commit

--- AddDevicesFromFile.py
+++ AddDevicesFromFile.py
@@ -5,8 +5,6 @@
 ## This Script Will add Devices to Netbox by their Serialnumber and DeviceType
 ## They will be stored in a site called Inventory 
 
-
-
 from dcim.choices import DeviceStatusChoices
 from dcim.models import Device,DeviceRole,DeviceType,Site
 from extras.scripts import *
@@ -16,21 +14,22 @@ class Add_Devices(Script):
     class Meta:
         name= "Add New Devices From File"
         description= "Adding new Devices to Inventory with Status OK"
-        Field_Order=["InputFileOfSerialNumbers","TypeOfDevice"]
+        Field_Order=["InputFile_Of_SerialNumbers","Type_Of_Device"]
 
-    InputFileOfSerialNumbers = FileVar(
+    InputFile_Of_SerialNumbers = FileVar(
         description="Add the file of Serial Numbers",
         required=True
     
     )
-    TypeOfDevice= ObjectVar(
-        description = "Choose which Device type you want to add",
-        model=DeviceType
+    Type_Of_Device= ObjectVar(
+        description = "Choose which Device the serial numbers represent",
+        model=DeviceType,
+        display_field="model"
     )
     
 
     def run(self,data,commit):
-        ListOfSerialNumbers = data["InputFileOfSerialNumbers"].read().decode("utf-8").strip()
+        ListOfSerialNumbers = data["InputFile_Of_SerialNumbers"].read().decode("utf-8").strip()
         
         for i in range(len(ListOfSerialNumbers)):     
             Create_Device= Device(
