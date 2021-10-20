ae3ec80740cb2396f10c9406ffc3fc8311cc79d6
virrelee <viktor.lindgren@live.se>
2021-10-20 16:43:30 +0200

    commit

--- AddDevicesFromFile.py
+++ AddDevicesFromFile.py
@@ -29,7 +29,7 @@ class Add_Devices(Script):
     
 
     def run(self,data,commit):
-        ListOfSerialNumbers = data["InputFile_Of_SerialNumbers"].read().decode("utf-8").strip("\n")
+        ListOfSerialNumbers = data["InputFile_Of_SerialNumbers"].readlines().decode("utf-8").strip()
         
         for i in range(len(ListOfSerialNumbers)):     
             Create_Device= Device(
