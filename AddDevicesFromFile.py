db3b7757075603d0d5b61bce5fa1a22940840fa1
virrelee <viktor.lindgren@live.se>
2021-10-22 18:25:09 +0200

    commit

--- AddDevicesFromFile.py
+++ AddDevicesFromFile.py
@@ -50,7 +50,7 @@ class Add_Devices(Script):
             Interface = InterfaceTemplate(
                 name="MGMT"
             )
-            Interface.instantiate(Device.objects.get(name=Create_Device))
+            Interface.instantiate(Device.objects.get(serial=Create_Device.serial))
             Interface.save()
             self.log_success(f"Created New Device with serial-Number {ListOfSerialNumbers[i]}")
         
