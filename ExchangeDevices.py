d5b2f539579676d4dd14a07db1190ad717f6a221
virrelee <viktor.lindgren@live.se>
2021-10-22 22:09:45 +0200

    commit

--- ExchangeDevices.py
+++ ExchangeDevices.py
@@ -43,7 +43,7 @@ class ExchangeDevices(Script):
 
         ipa = IPAddress.objects.get(address="10.160.199.15/24")
         ipa.assigned_object=newdevice
-        ipa.assigned_object_id=Interface.objects.get(id=newdevice.id)
+        ipa.assigned_object_id=140
         ipa.save()
 
         #IP = IPAddress(
