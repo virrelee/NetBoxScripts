62cf302ac9a0a3a1568901e2224344668b833d48
virrelee <viktor.lindgren@live.se>
2021-10-24 16:36:28 +0200

    commit

--- ExchangeDevices.py
+++ ExchangeDevices.py
@@ -41,6 +41,7 @@ class ExchangeDevices(Script):
         newdevice.tenant=oldevice.tenant
         newdevice.status=DeviceStatusChoices.STATUS_ACTIVE
         newdeviceinterfaceId = Interface.objects.get(device=newdevice.id)
+        print (newdeviceinterfaceId)
         newdeviceIP_ID = Device.objects.filter(name=newdevice).values_list("primary_ip4", flat=True).first()
         ipa = IPAddress.objects.get(id=newdeviceIP_ID)
         #ipa.assigned_object_type=ContentType.objects.filter(model="interface").id
