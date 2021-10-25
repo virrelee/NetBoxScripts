bf17846c4061e44effc30f2fbf973dd8f29d0d49
virrelee <viktor.lindgren@live.se>
2021-10-25 20:46:00 +0200

    commit

--- CpDevicesFromFile.py
+++ CpDevicesFromFile.py
@@ -116,7 +116,7 @@ class CpDevicesFromFile(Script):
                         self.log_success(f"Created New Site {siteObject.name}")
                         return (siteObject.name)
                     except ObjectDoesNotExist as error:
-                        self.log_info(error)
+                        self.log_warning(error)
                         pass
 
 
