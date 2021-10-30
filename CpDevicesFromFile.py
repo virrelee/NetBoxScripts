54c82ccf8d4e91826621bdde53cce7aab911fe07
virrelee <viktor.lindgren@live.se>
2021-10-30 20:10:51 +0200

    commit

--- CpDevicesFromFile.py
+++ CpDevicesFromFile.py
@@ -313,7 +313,7 @@ class CpDevicesFromFile(Script):
 
                     
                     if deviceObject.rack is not nan:
-                        device.rack=Rack.objects.filter(site=deviceObject.site).filter(name=deviceObject.name)
+                        device.rack=Rack.objects.filter(site=deviceObject.site).get(name=deviceObject.name)
 
                     if deviceObject.status is not nan:
                         device.status=DeviceStatusChoices.STATUS_ACTIVE
