46d77183c33bdb62e86625c1a3ced817ca22e25c
virrelee <viktor.lindgren@live.se>
2021-10-30 21:32:30 +0200

    commit

--- CpDevicesFromFile.py
+++ CpDevicesFromFile.py
@@ -27,7 +27,7 @@ class CpDevicesFromFile(Script):
                 slugname= slugish
                 randslug = str(randint(0,1000))
                 slugname+=randslug
-                if Device.objects.filter(slug=slugname).exists():
+                if Device.objects.filter(asset_tag=slugname).exists():
                     pass
                 else:
                     break
